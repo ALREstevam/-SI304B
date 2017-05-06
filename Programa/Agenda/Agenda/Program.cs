1aaee8cdcc879904de99244ba0cca44c5e4cfe26
ALREstevam <a166348@g.unicamp.com>
2017-05-06 18:46:10 -0300

    Commit adicional

--- Programa/Agenda/Agenda/Program.cs
+++ Programa/Agenda/Agenda/Program.cs
@@ -16,7 +16,7 @@ namespace Agenda
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainAppAgenda());
+            Application.Run(new AgendaTestForm());
         }
     }
 }
