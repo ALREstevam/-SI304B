0108adab5e5578f2775bcdcfa254a0e52ab90d1d
ALREstevam <a166348@g.unicamp.com>
2017-05-06 18:45:20 -0300

    Incluindo exemplo simples com SQLite
    Criar banco
    Inserir
    Acessar

---
+++ Programa/Agenda/Agenda/AgendaTestForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Agenda
+{
+    partial class AgendaTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.taskName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Valor";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.taskName});
+            this.dataGridView1.Location = new System.Drawing.Point(93, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(466, 313);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // taskName
+            // 
+            this.taskName.HeaderText = "taskName";
+            this.taskName.Name = "taskName";
+            this.taskName.ReadOnly = true;
+            // 
+            // AgendaTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 546);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AgendaTestForm";
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn taskName;
+    }
+}
+
---
+++ Programa/Agenda/Agenda/AgendaTestForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Agenda
+{
+
+    public partial class AgendaTestForm : Form
+    {
+        //Classe tarefa
+        public class Task{
+            public int id {get; set;}
+            public string taskName { get; set; }
+        }
+
+        public static string connectionStr = "Data Source=Banco.db";
+        private static string dbName = "Banco.db";
+
+
+
+        private void LoadTasks()
+        {
+            SQLiteConnection conn = new SQLiteConnection(connectionStr);
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM tb_tasks", conn);
+            SQLiteDataReader dr = cmd.ExecuteReader();
+            List<Task> lista = new List<Task>();
+
+            while (dr.Read())
+            {
+                lista.Add(new Task
+                {
+                    id = Convert.ToInt32(dr["id_task"]),
+                    taskName = dr["name_task"].ToString()
+                });
+            }
+            dataGridView1.DataSource = lista;
+
+        }
+
+        private void executeDefault()
+        {
+
+        }
+
+
+        public AgendaTestForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Criação do banco de dados
+            if (!File.Exists(dbName)) {
+                SQLiteConnection.CreateFile(dbName);
+                SQLiteConnection conn = new SQLiteConnection(connectionStr);
+                conn.Open();
+
+                StringBuilder sql = new StringBuilder();
+                sql.AppendLine("CREATE TABLE IF NOT EXISTS tb_tasks ([id_task] INTEGER PRIMARY KEY AUTOINCREMENT,");
+                sql.AppendLine("[name_task] VARCHAR(30) NOT NULL)");
+
+                SQLiteCommand cmd = new SQLiteCommand(sql.ToString(), conn);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao criar banco de dados: " + ex.Message);
+                }
+            }
+
+
+            LoadTasks();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ 
+            SQLiteConnection conn = new SQLiteConnection(connectionStr);
+
+            if(conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SQLiteCommand cmd = new SQLiteCommand("INSERT INTO tb_tasks (name_task) VALUES (@name_task)", conn);
+            cmd.Parameters.AddWithValue("name_task", textBox1.Text.Trim());
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Inserido com sucesso");
+                textBox1.Text = string.Empty;
+                LoadTasks();
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar registro " + ex.Message);
+            }
+        }
+
+        
+
+
+    }
+}
