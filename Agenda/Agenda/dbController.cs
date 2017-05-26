7792bf954d15474fb9b63deeaf7cb9840b4c18f7
ALREstevam <a166348@g.unicamp.com>
2017-05-26 08:59:57 -0300

    Adicionando classes

---
+++ Agenda/Agenda/dbController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class dbController
+{
+    private string filename;
+
+	public dbController(string filename)
+	{
+        this.filename = filename;
+	}
+
+    public bool createDb()
+    {
+
+    }
+}
